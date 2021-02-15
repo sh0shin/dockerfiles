11a177f7ec38ef344c2b23235c12747eceff6393
Chris Frage <git@sh0shin.org>
2021-02-15 22:32:03 +0100

    feat: hacky build script

---
+++ xbin/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# vim: set ft=sh :
+
+docker_root="$(git rev-parse --show-toplevel)"
+docker_space="sh0shin"
+
+while read -r -e -d '' docker_file
+do
+  docker_fullpath="${docker_file%/Dockerfile}"
+  docker_path="${docker_fullpath//$docker_root\//}"
+  docker_name="${docker_path//\//-}"
+  (
+    cd "$docker_fullpath" || return
+    docker build -t "$docker_space/$docker_name" .
+    echo docker push "$docker_space/$docker_name"
+  )
+done < <(find "$docker_root" -type f -name 'Dockerfile' -print0 | sort -z)
